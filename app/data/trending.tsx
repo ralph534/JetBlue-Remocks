615bfe06994d9afa1589e739577380be25bf1bfb
Ralph Etienne <ralphyetienne@yahoo.com>
2024-04-25 02:29:55 -0400

    created dummy data for trending places and implemented a floating airple with framer-motion

---
+++ app/data/trending.tsx
@@ -0,0 +1,23 @@
+export const trending_data = [
+    {
+        id: 1,
+        src: "https://www.planetware.com/photos-large/JPN/japan-tokyo-asakusa-senso-ji-temple.jpg",
+        title: "Tokyo",
+        location: "China",
+        description: "32 deals"
+    },
+    {
+        id: 2,
+        src: "https://www.holidify.com/images/cmsuploads/compressed/shutterstock_1196820841_20190903122749.jpg",
+        title: "Tokyo",
+        location: "China",
+        description: "32 deals"
+    },
+    {
+        id: 3,
+        src: "https://www.planetware.com/photos-large/INA/bali-pura-ulun-danu.jpg",
+        title: "Tokyo",
+        location: "China",
+        description: "32 deals"
+    },
+]
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,12 @@ import background from "/public/jetBlue.jpeg"
 import booking from './booking.jsx'
 import { slideAnimation } from "@/lib/config/motion.js";
 import { MotionDiv } from "@/components/MotionDiv.tsx";
-import jetblueside from "/public/blue-sapphire-removebg-preview.png"
+import jetblueside from "/public/takeflightJetblue-removebg-preview.png"
+import attendent from "/public/jetblueattendant-removebg-preview.png"
 import cloud from "/public/cloudspng.png"
+import { CAlert } from '@coreui/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import  {trending_data}  from "./data/trending";
 
 
 export default function Home() {
@@ -16,7 +20,7 @@ export default function Home() {
 
 
   return (
-    <main className="flex flex-col item-center justify-between">
+    <main className="flex flex-col justify-between">
       <div className="relative w-full">
         <div className="absolute -z-10 w-full">
           <Image src={background} alt="backgroundimage" className="w-full opacity-50" width={1000} height={1000}/>
@@ -24,14 +28,36 @@ export default function Home() {
         <section className="m-w-7xl mx-auto lg:px-8 flex flex-col py-2 items-center justify-center">
           <h2 className="font-bold text-4xl text-white ">It's so fly to see you.</h2>
           <MotionDiv
-          {...slideAnimation("right")}>
-            <div className="relative w-[860px] h-[500px]">
-              <Image src={jetblueside}  width={200} height={200} alt="plane" className="absolute mb-14 inset-0 w-full h-full object-cover" />
-              <Image src={cloud} width={300} height={400} alt="cloud" className="absolute inset-0 mt-[100px] w-full h-full object-cover"/>
-              <Image src={cloud} width={300} height={400} alt="cloud" className="absolute inset-0 mt-[100px] w-full h-full object-cover"/>
-            </div>
+          {...slideAnimation("left")}
+
+          >
+            <MotionDiv
+            initial={{
+            transform: "translateZ(-12px) translateY(-92px)",
+          }}
+          animate={{
+            transform: "translateZ(-22px) translateY(-18px)",
+          }}
+          transition={{
+            repeat: Infinity,
+            repeatType: "mirror",
+            duration: 3,
+            ease: "easeInOut",
+          }}>
+            <div className="relative w-[900px] h-[400px]">
+              <Image src={jetblueside}  width={300} height={300} alt="plane" className="absolute mb-14 inset-0 w-[900px] h-[400px] object-cover"/>
+              </div>
+            </MotionDiv>
           </MotionDiv>
-          <div className=" absolute w-[600px] rounded-md flex items-center bg-[#01205B] justify-between border-b-2">
+          <div className="absolute flex">
+              <MotionDiv {...slideAnimation("up")} className="-z-10">
+                <Image src={attendent} alt="attendent" width={900} height={900} className="ml-2 "/>
+              </MotionDiv>
+              <Image src={cloud} width={200} height={200} alt="cloud" className="absolute opacity-50 inset-0 mt-[30px] w-[1250px] h-[350px] object-cover"/>
+              <Image src={cloud} width={200} height={200} alt="cloud" className="absloute opacity-50 inset-0 mt-[30px] w-[1250px] h-[350px] object-cover"/>
+              <Image src={cloud} width={200} height={200} alt="cloud" className="absolute opacity-50 inset-0 mt-[30px] w-[1250px] h-[350px] object-cover"/>
+            </div>
+          <div className=" absolute w-[600px] rounded-md  mt-8 flex items-center lg:px-8 p-2 bg-[#01205B] justify-between">
         <a href="#" className="text-sm font-light leading-6 text-white  
           border-b-4 border-transparent hover:border-orange-500 transition-colors">
           Flights
@@ -46,21 +72,39 @@ export default function Home() {
         </a>
         <a href="#" className="text-sm font-light leading-6 text-white 
           border-b-4 border-transparent hover:border-orange-500 transition-colors">
-          Flights + Cruise
+          Cars
         </a>
         <a href="#" className="text-sm font-light leading-6 text-white 
           border-b-4 border-transparent hover:border-orange-500 transition-colors">
-          Flights + Cruise
+          Stays
         </a>
       </div>
         </section>
       </div>
       <section className="m-4 mt-0 -mb-14 px-2 lg:px-4">
+        
 
       </section>
 
-      <section>
+      <section className="mx-auto max-w-7xl mt-10 p-6 bg-white rounded-t-lg">
+          <div className="pt-5">
+              <h3 className="text-xl font-bold">Trending Destinations</h3>
+              <p className="font-light">Most popular choices for travelers from around the world</p>
+          </div>
 
+          <div className="flex space-x-4 py-5 overflow-x-scoll">
+              {trending_data.map(item => (
+                <div key={item.id} className="space-y-1 shrink-0 cursor-pointer">
+                    <img key={item.id} 
+                         className="w-80 h-72 object-cover rounded-lg pb-2"
+                         src={item.src}
+                         alt=""/>
+                     <p className="font-bold">{item.title}</p>
+                     <p>{item.location}</p>
+                     <p className="font-light text-sm">{item.description}</p>
+                </div>
+              ))}
+          </div>
       </section>
     </main>
   );
